c1218f4f4ff2bf440206611fea6eaf1be5c92f4d
Berry van Mondfrans <berryvanmondfrans@gmail.com>
2017-07-24 18:41:07 +0200

    Added #5

---
+++ Package/Converters/AmpAudioConverter.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gastror
+ * Date: 18-7-17
+ * Time: 21:11
+ */
+
+namespace Amplifier\Package\Converters;
+
+
+use Amplifier\Package\ConverterBaseTrait;
+use Amplifier\Package\ConverterInterface;
+
+/**
+ * Class AmpVideoConverter
+ * @package Amplifier\Package\Converters
+ * @property \DOMDocument $Doc
+ * @mixin ConverterBaseTrait
+ */
+class AmpAudioConverter implements ConverterInterface
+{
+    protected $Attributes = [];
+
+    use ConverterBaseTrait;
+    /** {@inheritdoc} */
+    public function convert(): ConverterInterface
+    {
+        $oAudio = $this->Doc->firstChild; // Load the element
+        $this->_extractAttributes($oAudio);
+        $oAmpVideo = new \DOMElement("amp-audio");
+        $oNoScript = new \DOMElement("noscript");
+        @$this->Doc->loadHTML("",  LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        $oPLaceholderNoscript = new \DOMElement("p");
+        $oPLaceholderNoscript->appendChild(new \DOMText("Your browser either does not support JavaScript or HTML5 Audio."));
+        $this->Doc->appendChild($oPLaceholderNoscript);
+        foreach ($this->Attributes as $sName => $sValue) {
+            $oAmpVideo->setAttribute($sName, $sValue);
+        }
+        $oAmpVideo->appendChild($oNoScript);
+        $oNoScript->setAttribute("fallback", "");
+        $oNoScript->appendChild($oAudio);
+
+        return $this;
+    }
+
+}
